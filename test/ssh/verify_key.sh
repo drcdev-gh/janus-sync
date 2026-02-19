b1ad66af741d4c8706dabc0010574eebb94a3a99
drcdev-gh <631758+drcdev-gh@users.noreply.github.com>
2026-02-19 09:48:58 +0900

    remove comment

--- test/ssh/verify_key.sh
+++ test/ssh/verify_key.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-#exec /usr/bin/curl -s -G \
-#  --data-urlencode "pubkey=$1" \
-#  -H "x-api-key: helloworld" \
-#  http://janus:8085/ssh/validate
 
 curl -s -G --data-urlencode "pubkey=$1 $2" -H "x-api-key:helloworld" http://janus-sync-janus-sync-1:8085/ssh/validate
